62c91ec81a91f65d7eb7294a44ae266c19a22057
akhmedov <akhmedov@e43e0fea-2032-0410-9da9-add0f311814c>
2008-01-26 18:41:16 +0000

    Issue 38

--- openconstructor/data/hybrid/i_hybrid.php
+++ openconstructor/data/hybrid/i_hybrid.php
@@ -1,23 +1,23 @@
 <?php
 /**
  * Copyright 2003 - 2007 eSector Solutions, LLC
- * 
+ *
  * All rights reserved.
- * 
+ *
  * This file is part of Open Constructor (http://www.openconstructor.org/).
- * 
+ *
  * Open Constructor is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation.
- * 
+ *
  * Open Constructor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * The GNU General Public License can be found at
  * http://www.gnu.org/copyleft/gpl.html
- * 
+ *
  * $Id: i_hybrid.php,v 1.11 2007/05/15 23:05:20 sanjar Exp $
  */
 require_once($_SERVER['DOCUMENT_ROOT'].'/openconstructor/lib/wccommons._wc');
@@ -31,41 +31,41 @@ switch(@$_POST['action'])
 	case 'add_field':
 		$ds = &$dsm->load(@$_POST['ds_id']);
 		assert($ds->ds_id > 0 && @$_POST['fieldclass'] != '' && utf8_strpos($_POST['fieldclass'], '/') === false);
-		
+
 		require_once(LIBDIR.'/hybrid/fields/'.$_POST['fieldclass'].'field._wc');
 		switch ($_POST['fieldclass']) {
 			case 'primitive':
 				$f = new PrimitiveField(@$_POST['key'],@$_POST['header'],@$_POST['type'],@$_POST['isRequired']=='true');
 			break;
-			
+
 			case 'document':
 				$f = new DocumentField(@$_POST['key'],@$_POST['header'],@$_POST['type'],@$_POST['isRequired']=='true',@$_POST['document_isown']=='true',@$_POST['fromDS']);
 			break;
-			
+
 			case 'array':
 				$f = new ArrayField(@$_POST['key'],@$_POST['header'],@$_POST['type'],@$_POST['isRequired']=='true',@$_POST['array_isown']=='true',@$_POST['fromDS']);
 			break;
-			
+
 			case 'datasource':
 				$f = new DatasourceField(@$_POST['key'],@$_POST['header'],@$_POST['type'],@$_POST['fromDS']);
 			break;
-			
+
 			case 'tree':
 				$f = new TreeField(@$_POST['key'],@$_POST['header'],@$_POST['tree_type'], @$_POST['isRequired']=='true', @$_POST['tree_is_array'] == 'true');
 			break;
-			
+
 			case 'enum':
 				$f = new EnumField(@$_POST['key'],@$_POST['header'],@$_POST['enum_type'], @$_POST['isRequired']=='true', @$_POST['enum_is_array'] == 'true');
 			break;
-			
+
 			case 'file':
 				$f = new FileField(@$_POST['key'],@$_POST['header'],@$_POST['file_types'], @$_POST['isRequired']=='true');
 			break;
-			
+
 			case 'rating':
 				$f = new RatingField(@$_POST['key'], @$_POST['header'], @$_POST['rating_type']);
 			break;
-			
+
 			default:
 				assert(true == false);
 		}
@@ -73,12 +73,12 @@ switch(@$_POST['action'])
 		if($res)
 			echo '<script>try{window.opener.location.href=window.opener.location.href;}catch(RuntimeException){}location.href="field/'.$_POST['fieldclass'].'.php?id='.$res.'";</script>Loading...';
 	break;
-		
+
 	case 'edit_field':
 		require_once(LIBDIR.'/hybrid/fields/fieldfactory._wc');
 		$f = FieldFactory::getField(@$_POST['id']);
 		$old = $f;
-		
+
 		switch ($f->family) {
 			case 'primitive':
 				$f->header = @$_POST['header'];
@@ -93,40 +93,40 @@ switch(@$_POST['action'])
 
 			case 'file':
 				$f->header = @$_POST['header'];
-				if((int)@$_POST['maxSize']) 
+				if((int)@$_POST['maxSize'])
 					$f->maxSize = (int)@$_POST['maxSize'];
 				$f->setImgBounds(@$_POST['imgBounds']);
 				$f->setTypes(@$_POST['file_types']);
 				$f->isRequired = @$_POST['isRequired'] == 'true';
 			break;
-			
+
 			case 'document':
 				$f->header = @$_POST['header'];
 				$f->isRequired = @$_POST['isRequired'] == 'true';
 			break;
-			
+
 			case 'array':
 				$f->header = @$_POST['header'];
 				$f->isRequired = @$_POST['isRequired'] == 'true';
 			break;
-			
+
 			case 'datasource':
 				$f->header = @$_POST['header'];
 				$f->isRequired = @$_POST['isRequired'] == 'true';
 			break;
-			
+
 			case 'tree':
 				$f->header = @$_POST['header'];
 				$f->isArray = @$_POST['tree_is_array'] == 'true';
 				$f->isRequired = @$_POST['isRequired'] == 'true';
 			break;
-			
+
 			case 'enum':
 				$f->header = @$_POST['header'];
 				$f->isArray = @$_POST['enum_is_array'] == 'true';
 				$f->isRequired = @$_POST['isRequired'] == 'true';
 			break;
-			
+
 			case 'rating':
 				$f->header = @$_POST['header'];
 				$f->isRequired = @$_POST['isRequired'] == 'true';
@@ -135,17 +135,17 @@ switch(@$_POST['action'])
 		FieldFactory::updateField($old, $f);
 		header('Location: '.$_SERVER['HTTP_REFERER']);
 	break;
-		
+
 	case 'remove_field':
 		$ds = &$dsm->load(@$_POST['ds_id']);
 		assert($ds->ds_id > 0);
-		
+
 		if(@sizeof($_POST['field']))
 			FieldFactory::removeFields($ds->getRecord(), $_POST['field']);
-			
+
 		header('Location: '.$_SERVER['HTTP_REFERER']);
 	break;
-		
+
 	case 'create_hybrid':
 		require_once(LIBDIR.'/wcdatasource._wc');
 		require_once(LIBDIR.'/hybrid/hybriddocument._wc');
@@ -157,7 +157,7 @@ switch(@$_POST['action'])
 			WCS::runAs(WCS_ROOT_ID);
 		}
 		$ds = &$dsm->load($_POST['ds_id']);
-		
+
 		$doc = $ds->getEmptyDocument();
 		$doc->readValues($_POST);
 		$doc->readFiles($_FILES);
@@ -184,7 +184,7 @@ switch(@$_POST['action'])
 		require_once(LIBDIR.'/hybrid/hybriddocument._wc');
 		assert(isset($_POST['header']) && trim($_POST['header']) !='' && @$_POST['ds_id'] > 0);
 		$ds = &$dsm->load($_POST['ds_id']);
-		
+
 		$doc = $ds->getEmptyDocument();
 		$doc->readValues($_POST);
 		$doc->readFiles($_FILES);
@@ -194,17 +194,15 @@ switch(@$_POST['action'])
 	break;
 
 	case 'delete_hybrid':
-		if(isset($_POST['ds_id']) && isset($_POST['ids']))
-		{
-			require_once(LIBDIR.'/hybrid/dshybrid._wc');
+		if(isset($_POST['ds_id']) && isset($_POST['ids'])) {
 			require_once(LIBDIR.'/hybrid/dshybridfactory._wc');
-			$ds=new DSHybridFactory();
+			$ds = new DSHybridFactory();
 			$ds->removeDocuments(&$_POST['ids']);
 		}
 //		header('Location: '.$_SERVER['HTTP_REFERER']);
 		die('<meta http-equiv="Refresh" content="0; URL='.$_SERVER['HTTP_REFERER'].'">');
 	break;
-	
+
 	case 'remove_ds':
 		if(isset($_POST['ds_id']))
 		{
@@ -216,7 +214,7 @@ switch(@$_POST['action'])
 		}
 		die('<meta http-equiv="Refresh" content="0; URL=http://'.$_host.WCHOME.'/data/">');
 	break;
-	
+
 	case 'move_documents':
 		assert(isset($_POST['ds_id']) && isset($_POST['dest_ds_id']) && isset($_POST['ids']));
 		require_once(LIBDIR.'/hybrid/dshybrid._wc');
@@ -229,7 +227,7 @@ switch(@$_POST['action'])
 		} else
 			echo '<p><a href="'.$_SERVER['HTTP_REFERER'].'">Back</a>';
 	break;
-	
+
 	case 'publish_documents':
 		if(isset($_POST['ds_id']) && isset($_POST['ids']))
 		{
@@ -240,7 +238,7 @@ switch(@$_POST['action'])
 //		header('Location: '.$_SERVER['HTTP_REFERER']);
 		die('<meta http-equiv="Refresh" content="0; URL='.$_SERVER['HTTP_REFERER'].'">');
 	break;
-	
+
 	case 'unpublish_documents':
 		if(isset($_POST['ds_id']) && isset($_POST['ids']))
 		{
